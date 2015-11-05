2e797088fe4e53f651e98aa551411dea8c341e3a
PavloBorysenko <irod-09@mail>
2015-11-05 10:47:52 +0200

    List Employers

---
+++ BorysenkoFormEmplo/BorysenkoFormEmplo/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace BorysenkoFormEmplo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.proiz = new System.Windows.Forms.RadioButton();
+            this.inge = new System.Windows.Forms.RadioButton();
+            this.mark = new System.Windows.Forms.RadioButton();
+            this.listEmpl = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.proiz);
+            this.groupBox1.Controls.Add(this.inge);
+            this.groupBox1.Controls.Add(this.mark);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(138, 116);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Отделы";
+            // 
+            // proiz
+            // 
+            this.proiz.AutoSize = true;
+            this.proiz.Location = new System.Drawing.Point(7, 93);
+            this.proiz.Name = "proiz";
+            this.proiz.Size = new System.Drawing.Size(124, 17);
+            this.proiz.TabIndex = 2;
+            this.proiz.TabStop = true;
+            this.proiz.Text = "Производственный";
+            this.proiz.UseVisualStyleBackColor = true;
+            this.proiz.CheckedChanged += new System.EventHandler(this.proiz_CheckedChanged);
+            // 
+            // inge
+            // 
+            this.inge.AutoSize = true;
+            this.inge.Location = new System.Drawing.Point(7, 55);
+            this.inge.Name = "inge";
+            this.inge.Size = new System.Drawing.Size(91, 17);
+            this.inge.TabIndex = 1;
+            this.inge.TabStop = true;
+            this.inge.Text = "Инженерный";
+            this.inge.UseVisualStyleBackColor = true;
+            this.inge.CheckedChanged += new System.EventHandler(this.inge_CheckedChanged);
+            // 
+            // mark
+            // 
+            this.mark.AutoSize = true;
+            this.mark.Location = new System.Drawing.Point(7, 20);
+            this.mark.Name = "mark";
+            this.mark.Size = new System.Drawing.Size(106, 17);
+            this.mark.TabIndex = 0;
+            this.mark.TabStop = true;
+            this.mark.Text = "Маркетинговый";
+            this.mark.UseVisualStyleBackColor = true;
+            this.mark.CheckedChanged += new System.EventHandler(this.mark_CheckedChanged);
+            // 
+            // listEmpl
+            // 
+            this.listEmpl.FormattingEnabled = true;
+            this.listEmpl.Location = new System.Drawing.Point(12, 145);
+            this.listEmpl.Name = "listEmpl";
+            this.listEmpl.Size = new System.Drawing.Size(138, 199);
+            this.listEmpl.TabIndex = 1;
+            this.listEmpl.SelectedIndexChanged += new System.EventHandler(this.listEmpl_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(371, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 198);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Location = new System.Drawing.Point(281, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 65);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.Location = new System.Drawing.Point(553, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 65);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.Color.Red;
+            this.textBox2.Location = new System.Drawing.Point(371, 229);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(165, 74);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(281, 215);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Показать";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.ClientSize = new System.Drawing.Size(631, 355);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listEmpl);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton proiz;
+        private System.Windows.Forms.RadioButton inge;
+        private System.Windows.Forms.RadioButton mark;
+        private System.Windows.Forms.ListBox listEmpl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ BorysenkoFormEmplo/BorysenkoFormEmplo/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BorysenkoFormEmplo
+{
+    public partial class Form1 : Form
+    {
+        int index;
+        class Person
+        {
+            public string name;
+            public string s_name;
+            public string b_day;
+            public Person(string N, string S, string D)
+            {
+                name = N;
+                s_name = S;
+                b_day = D;
+            }
+        }
+        class Emploer : Person
+        {
+            public int id;
+            public string prof;
+            public Emploer(string N, string S, string D, int I, string P)
+                : base(N, S, D)
+            {
+                id = I;
+                prof = P;
+            }
+            public string print_inf() {
+                string str = name + "\r\n ______________\r\n" + s_name + "\r\n ______________\r\n" + "\r\nПрофессия\r\n" + prof + "\r\n ______________\r\n";
+                return str;
+            }
+            public string print_dop_inf() {
+                string str = "День рождения " + b_day + "\r\n ______________\r\n" + "Инд. Код " + id + "\r\n ______________\r\n";
+                return str;
+            }
+        }
+        class Market : Emploer
+        {
+            public int zp;
+
+            public Market(string N, string S, string D, int I, int Z)
+                : base(N, S, D, I, "Маркетолог")
+            {
+                zp = Z;
+            }
+            public string info() {
+                return print_inf() + "\nЗарплата \n" + zp;
+            }
+        }
+
+        class Ingener : Emploer
+        {
+            public int zp;
+
+            public Ingener(string N, string S, string D, int I, int Z)
+                : base(N, S, D, I, "Инженер")
+            {
+                zp = Z;
+            }
+            public string info()
+            {
+                return print_inf() + "\nЗарплата \n" + zp;
+            }
+        }
+        class Proizv : Emploer
+        {
+            public int zp;
+
+            public Proizv(string N, string S, string D, int I, int Z)
+                : base(N, S, D, I, "Рабочий")
+            {
+                zp = Z;
+            }
+           
+            public string info()
+            {
+                return print_inf() + "\nЗарплата \n" + zp;
+            }
+        }
+
+        List<Emploer> market = new List<Emploer>();
+        List<Emploer> ingener = new List<Emploer>();
+        List<Emploer> rabot = new List<Emploer>();
+        List<Emploer> tempMas = new List<Emploer>();
+
+        public string print(int i) {
+            if (tempMas[i] is Market) {
+                return (tempMas[i] as Market).info();
+            }
+            else if (tempMas[i] is Ingener) {
+                return (tempMas[i] as Ingener).info();
+            }
+            else if (tempMas[i] is Proizv)
+            {
+                return (tempMas[i] as Proizv).info();
+            }
+            return "";
+        
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            market.Add(new Market("Василий","Продажкин","12.02.93",121211,1000));
+            market.Add(new Market("Сергей", "Рекламов", "09.12.89", 123211, 1100));
+            market.Add(new Market("Антон", "Шефко", "31.04.75", 909754, 1500));
+            market.Add(new Market("Елена", "Кофеварко", "21.05.95", 673531, 800));
+
+            ingener.Add(new Ingener("Виталий","Умников","07.10.90",3689428,1300));
+            ingener.Add(new Ingener("Дмитрий", "Сыншефко", "09.08.96", 858878, 2000));
+            ingener.Add(new Ingener("Лев", "Изобретенидзе", "01.01.91", 844884, 1450));
+            ingener.Add(new Ingener("Светлана", "Боссиня", "06.11.80", 4747755, 1800));
+
+            rabot.Add(new Proizv("Лена", "Пролова", "09.01.95", 6444353, 700));
+            rabot.Add(new Proizv("Виталий", "Слесарев", "07.05.80", 112211, 800));
+            rabot.Add(new Proizv("Антон", "Мастеров", "05.09.93", 456789, 1000));
+            rabot.Add(new Proizv("Герман", "Ремонтин", "11.10.78", 763664, 800));
+
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void mark_CheckedChanged(object sender, EventArgs e)
+        {
+            var radio = (RadioButton)sender;
+            if (radio.Checked)
+            {
+                index = 0;
+                tempMas = market;
+               
+                foreach (Market item in tempMas)
+                {
+                    listEmpl.Items.Add(item.name + " " + item.s_name);
+
+                }
+                listEmpl.SetSelected(index, true);
+                textBox1.Text = print(index);
+                textBox2.Text = tempMas[index].print_dop_inf();
+
+            }
+            else {
+                listEmpl.Items.Clear();
+            }  
+        }
+
+        private void inge_CheckedChanged(object sender, EventArgs e)
+        {
+            var radio1 = (RadioButton)sender;
+            if (radio1.Checked)
+            {
+                index = 0;
+                tempMas = ingener;
+               
+                foreach (Ingener item in tempMas)
+                {
+                    listEmpl.Items.Add(item.name + " " + item.s_name);
+                }
+                listEmpl.SetSelected(index, true);
+                textBox1.Text = print(index);
+                textBox2.Text = tempMas[index].print_dop_inf();
+            }
+            else
+            {
+                listEmpl.Items.Clear();
+            } 
+        }
+
+        private void proiz_CheckedChanged(object sender, EventArgs e)
+        {
+            var radio2 = (RadioButton)sender;
+            if (radio2.Checked)
+            {
+                index = 0;
+                tempMas = rabot;
+               
+                foreach (Proizv item in tempMas)
+                {
+                    listEmpl.Items.Add(item.name + " " + item.s_name);
+                   
+                } 
+                listEmpl.SetSelected(index, true);
+                    textBox1.Text = print(index);
+                    textBox2.Text = tempMas[index].print_dop_inf();
+
+            }
+            else
+            {
+                listEmpl.Items.Clear();
+            } 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            index++;
+            if (index > tempMas.Count-1)
+            {
+                index = 0;
+            }
+            listEmpl.SetSelected(index, true);
+            textBox1.Text = print(index);
+       
+            textBox2.Text = tempMas[index].print_dop_inf();
+        }
+
+        private void listEmpl_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectItem = listEmpl.SelectedItem.ToString();
+            index= listEmpl.FindString(selectItem);
+            textBox1.Text = print(index);
+          
+            textBox2.Text = tempMas[index].print_dop_inf();
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            var check = (sender as CheckBox);
+            if (check.Checked == true)
+            {
+                textBox2.Show();
+            }
+            else
+            {
+                textBox2.Hide();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            index--;
+            if (index < 0)
+            {
+                index = tempMas.Count-1;
+            }
+            listEmpl.SetSelected(index, true);
+            textBox1.Text = print(index);
+         
+            textBox2.Text = tempMas[index].print_dop_inf();
+            
+        }
+    }
+}
