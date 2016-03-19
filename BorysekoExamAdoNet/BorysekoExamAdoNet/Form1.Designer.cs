98573edcc0526dae23ea2e746ab334131376e9e8
PavloBorysenko <irod-09@mail>
2016-03-19 17:40:08 +0200

    Программа хранения паролей

---
+++ BorysekoExamAdoNet/BorysekoExamAdoNet/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace BorysekoExamAdoNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.TimeTable = new System.Windows.Forms.DataGridView();
+            this.InfoTimeTable = new System.Windows.Forms.DataGridView();
+            this.TablePlayer = new System.Windows.Forms.DataGridView();
+            this.TableInfoPlayer = new System.Windows.Forms.DataGridView();
+            this.etap1 = new System.Windows.Forms.ListBox();
+            this.etap2 = new System.Windows.Forms.ListBox();
+            this.etap3 = new System.Windows.Forms.ListBox();
+            this.etap4 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.TimeTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InfoTimeTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TablePlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TableInfoPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "load_click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TimeTable
+            // 
+            this.TimeTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TimeTable.Location = new System.Drawing.Point(1, 0);
+            this.TimeTable.Name = "TimeTable";
+            this.TimeTable.Size = new System.Drawing.Size(256, 165);
+            this.TimeTable.TabIndex = 1;
+            this.TimeTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TimeTable_CellContentClick);
+            // 
+            // InfoTimeTable
+            // 
+            this.InfoTimeTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.InfoTimeTable.Location = new System.Drawing.Point(1, 171);
+            this.InfoTimeTable.Name = "InfoTimeTable";
+            this.InfoTimeTable.Size = new System.Drawing.Size(408, 73);
+            this.InfoTimeTable.TabIndex = 2;
+            // 
+            // TablePlayer
+            // 
+            this.TablePlayer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TablePlayer.Location = new System.Drawing.Point(286, 0);
+            this.TablePlayer.Name = "TablePlayer";
+            this.TablePlayer.Size = new System.Drawing.Size(382, 165);
+            this.TablePlayer.TabIndex = 3;
+            // 
+            // TableInfoPlayer
+            // 
+            this.TableInfoPlayer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TableInfoPlayer.Location = new System.Drawing.Point(415, 171);
+            this.TableInfoPlayer.Name = "TableInfoPlayer";
+            this.TableInfoPlayer.Size = new System.Drawing.Size(358, 73);
+            this.TableInfoPlayer.TabIndex = 4;
+            this.TableInfoPlayer.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TableInfoPlayer_CellContentClick);
+            // 
+            // etap1
+            // 
+            this.etap1.FormattingEnabled = true;
+            this.etap1.Location = new System.Drawing.Point(1, 292);
+            this.etap1.Name = "etap1";
+            this.etap1.Size = new System.Drawing.Size(217, 43);
+            this.etap1.TabIndex = 5;
+            // 
+            // etap2
+            // 
+            this.etap2.FormattingEnabled = true;
+            this.etap2.Location = new System.Drawing.Point(1, 367);
+            this.etap2.Name = "etap2";
+            this.etap2.Size = new System.Drawing.Size(217, 43);
+            this.etap2.TabIndex = 6;
+            this.etap2.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // etap3
+            // 
+            this.etap3.FormattingEnabled = true;
+            this.etap3.Location = new System.Drawing.Point(1, 517);
+            this.etap3.Name = "etap3";
+            this.etap3.Size = new System.Drawing.Size(217, 43);
+            this.etap3.TabIndex = 7;
+            // 
+            // etap4
+            // 
+            this.etap4.FormattingEnabled = true;
+            this.etap4.Location = new System.Drawing.Point(1, 442);
+            this.etap4.Name = "etap4";
+            this.etap4.Size = new System.Drawing.Size(217, 43);
+            this.etap4.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-2, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Группа А";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-2, 501);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Группа D";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-2, 351);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Группа В";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-2, 426);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Группа С";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(224, 342);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(217, 43);
+            this.listBox1.TabIndex = 13;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(224, 459);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(217, 43);
+            this.listBox2.TabIndex = 14;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(448, 394);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(211, 56);
+            this.listBox3.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 588);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.etap4);
+            this.Controls.Add(this.etap3);
+            this.Controls.Add(this.etap2);
+            this.Controls.Add(this.etap1);
+            this.Controls.Add(this.TableInfoPlayer);
+            this.Controls.Add(this.TablePlayer);
+            this.Controls.Add(this.InfoTimeTable);
+            this.Controls.Add(this.TimeTable);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TimeTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.InfoTimeTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TablePlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TableInfoPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView TimeTable;
+        private System.Windows.Forms.DataGridView InfoTimeTable;
+        private System.Windows.Forms.DataGridView TablePlayer;
+        private System.Windows.Forms.DataGridView TableInfoPlayer;
+        private System.Windows.Forms.ListBox etap1;
+        private System.Windows.Forms.ListBox etap2;
+        private System.Windows.Forms.ListBox etap3;
+        private System.Windows.Forms.ListBox etap4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ BorysekoExamAdoNet/BorysekoExamAdoNet/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace BorysekoExamAdoNet
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        private MySqlConnection connection;
+        private string server = "localhost";
+        private string database = "hokey";
+        private string uid = "hokey";
+        private string password = "";
+        private string connectionString;
+
+        private BindingSource timeBindingSource = new BindingSource();
+        private BindingSource timeTimeInfoBindingSource = new BindingSource();
+        private BindingSource timePlayerBindingSource = new BindingSource();
+        private BindingSource PlayerInfoBindingSource = new BindingSource();
+
+       private DataSet data = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connectionString = "SERVER=" + server + ";DATABASE=" + database + ";UID=" + uid + ";PASSWORD=" + password + ";";
+
+                connection = new MySqlConnection(connectionString);
+
+                
+
+                MySqlDataAdapter masterDataAdapter = new MySqlDataAdapter("select * from time", connection);
+                masterDataAdapter.Fill(data, "time");
+
+                MySqlDataAdapter detailsDataAdapter = new MySqlDataAdapter("select * from infotim", connection);
+                detailsDataAdapter.Fill(data, "infotim");
+
+                MySqlDataAdapter playerDataAdapter = new MySqlDataAdapter("select * from player", connection);
+                playerDataAdapter.Fill(data, "player");
+
+                MySqlDataAdapter playerinfoDataAdapter = new MySqlDataAdapter("select * from infoplayer", connection);
+                playerinfoDataAdapter.Fill(data, "infoplayer");
+
+
+                MySqlDataAdapter turnirDataAdapter = new MySqlDataAdapter("select * from turnir", connection);
+                turnirDataAdapter.Fill(data, "turnir");
+
+
+                DataRelation relation = new DataRelation("timeInfo",
+                    data.Tables["time"].Columns["id"],
+                    data.Tables["infotim"].Columns["id_time"]);
+                data.Relations.Add(relation);
+
+                DataRelation relation3 = new DataRelation("timeturnir",
+                   data.Tables["time"].Columns["id"],
+                   data.Tables["turnir"].Columns["id_time1"]);
+                data.Relations.Add(relation3);
+
+                DataRelation relation4 = new DataRelation("timeturnir2",
+                   data.Tables["time"].Columns["id"],
+                   data.Tables["turnir"].Columns["id_time2"]);
+                data.Relations.Add(relation4);
+
+                DataRelation relation1 = new DataRelation("timePlayer",
+                   data.Tables["time"].Columns["id"],
+                   data.Tables["player"].Columns["id_time"]);
+                data.Relations.Add(relation1);
+
+                DataRelation relation2 = new DataRelation("PlayerInfo",
+              data.Tables["player"].Columns["id"],
+              data.Tables["infoplayer"].Columns["id_player"]);
+                data.Relations.Add(relation2);
+
+
+                timeBindingSource.DataSource = data;
+                timeBindingSource.DataMember = "time";
+
+                timeTimeInfoBindingSource.DataSource = timeBindingSource;
+                timeTimeInfoBindingSource.DataMember = "timeInfo";
+
+                timePlayerBindingSource.DataSource = timeBindingSource;
+                timePlayerBindingSource.DataMember = "timePlayer";
+
+                PlayerInfoBindingSource.DataSource = timePlayerBindingSource;
+                PlayerInfoBindingSource.DataMember = "PlayerInfo";
+
+                Print_etap("А", etap1);
+                Print_etap("В", etap2);
+                Print_etap("С", etap3);
+                Print_etap("D", etap4);
+            }
+            catch (MySqlException eee)
+            {
+                MessageBox.Show(eee.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TimeTable.DataSource = timeBindingSource;
+            InfoTimeTable.DataSource = timeTimeInfoBindingSource;
+            TablePlayer.DataSource = timePlayerBindingSource;
+            TableInfoPlayer.DataSource = PlayerInfoBindingSource;
+
+
+
+
+        }
+
+        private void TimeTable_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void TableInfoPlayer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        //void Print_finsl(string s1, string s2, int i, ListBox text)
+        //{
+
+        //    var query = from inf in data.Tables["infotim"].AsEnumerable()
+        //                from tur in data.Tables["turnir"].AsEnumerable()
+                   
+        //                where (string)tur["grup"] == s1
+        //                group inf by (int)inf["id_time"]== (int)tur["id_time1"] into m
+
+        //               let maxc=m.Max(inf=>(int)inf["count"])
+
+        //                select new { 
+        //                     Champion= m.Where(inf=>(int)inf["count"]==maxc)
+                        
+        //                };
+
+
+        
+        //} 
+        void Print_etap(string i, ListBox text) {
+
+            try
+            {
+                var query = from tim in data.Tables["time"].AsEnumerable()
+                            from tur in data.Tables["turnir"].AsEnumerable()
+                            from tim2 in data.Tables["time"].AsEnumerable()
+                            where (string)tur["grup"] == i
+                            where (int)tur["id_time1"] == (int)tim["id"]
+                            where (int)tur["id_time2"] == (int)tim2["id"]
+                            select  new {  Grup = tur["grup"], Time1 = tim["name"], Time2 = tim2["name"], Count1=tur["count1"], Count2=tur["count2"], Rez=tur["rez"]};
+
+
+                foreach (var etap in query)
+                {  string c ="Не сыгран";
+                if ((int)etap.Rez > 0)
+                {
+                    c = etap.Count1 + ":" + etap.Count1;
+                }
+                    string s = " " + etap.Time1 + " ^ " + etap.Time2 + "   " + c;
+                    text.Items.Add(s);
+                   
+                }
+            }
+            catch (MySqlException eee)
+            {
+                MessageBox.Show(eee.Message);
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
