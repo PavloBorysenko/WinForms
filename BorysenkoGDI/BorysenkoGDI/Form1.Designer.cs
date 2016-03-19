014c89c2e3d4c8672c80a71f5f53742f7ef36856
PavloBorysenko <irod-09@mail>
2016-03-19 18:05:38 +0200

    Программа рисования

---
+++ BorysenkoGDI/BorysenkoGDI/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace BorysenkoGDI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.заливкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.квадратToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.прямоугольникToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.элипсToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.линияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.цветToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.красныйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.синийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.зеленыйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.желтыйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.черныйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.белыйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.combo_w = new System.Windows.Forms.ComboBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem2,
+            this.заливкаToolStripMenuItem,
+            this.квадратToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.цветToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(837, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripMenuItem3.Image")));
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(38, 20);
+            this.toolStripMenuItem3.Text = " ";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripMenuItem2.Image")));
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItem2.Text = "   ";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // заливкаToolStripMenuItem
+            // 
+            this.заливкаToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("заливкаToolStripMenuItem.Image")));
+            this.заливкаToolStripMenuItem.Name = "заливкаToolStripMenuItem";
+            this.заливкаToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.заливкаToolStripMenuItem.Text = " ";
+            this.заливкаToolStripMenuItem.Click += new System.EventHandler(this.заливкаToolStripMenuItem_Click);
+            // 
+            // квадратToolStripMenuItem
+            // 
+            this.квадратToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.прямоугольникToolStripMenuItem,
+            this.элипсToolStripMenuItem,
+            this.линияToolStripMenuItem});
+            this.квадратToolStripMenuItem.Name = "квадратToolStripMenuItem";
+            this.квадратToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.квадратToolStripMenuItem.Text = "Фигуры";
+            // 
+            // прямоугольникToolStripMenuItem
+            // 
+            this.прямоугольникToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("прямоугольникToolStripMenuItem.Image")));
+            this.прямоугольникToolStripMenuItem.Name = "прямоугольникToolStripMenuItem";
+            this.прямоугольникToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.прямоугольникToolStripMenuItem.Text = "Прямоугольник";
+            this.прямоугольникToolStripMenuItem.Click += new System.EventHandler(this.прямоугольникToolStripMenuItem_Click);
+            // 
+            // элипсToolStripMenuItem
+            // 
+            this.элипсToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("элипсToolStripMenuItem.Image")));
+            this.элипсToolStripMenuItem.Name = "элипсToolStripMenuItem";
+            this.элипсToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.элипсToolStripMenuItem.Text = "Элипс";
+            this.элипсToolStripMenuItem.Click += new System.EventHandler(this.элипсToolStripMenuItem_Click);
+            // 
+            // линияToolStripMenuItem
+            // 
+            this.линияToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("линияToolStripMenuItem.Image")));
+            this.линияToolStripMenuItem.Name = "линияToolStripMenuItem";
+            this.линияToolStripMenuItem.Size = new System.Drawing.Size(163, 22);
+            this.линияToolStripMenuItem.Text = "Линия";
+            this.линияToolStripMenuItem.Click += new System.EventHandler(this.линияToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.BackColor = System.Drawing.Color.Black;
+            this.toolStripMenuItem1.Enabled = false;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(28, 20);
+            this.toolStripMenuItem1.Text = "   ";
+            // 
+            // цветToolStripMenuItem
+            // 
+            this.цветToolStripMenuItem.BackColor = System.Drawing.Color.White;
+            this.цветToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.красныйToolStripMenuItem,
+            this.синийToolStripMenuItem,
+            this.зеленыйToolStripMenuItem,
+            this.желтыйToolStripMenuItem,
+            this.черныйToolStripMenuItem,
+            this.белыйToolStripMenuItem});
+            this.цветToolStripMenuItem.Name = "цветToolStripMenuItem";
+            this.цветToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.цветToolStripMenuItem.Text = "Цвет";
+            // 
+            // красныйToolStripMenuItem
+            // 
+            this.красныйToolStripMenuItem.BackColor = System.Drawing.Color.Red;
+            this.красныйToolStripMenuItem.Name = "красныйToolStripMenuItem";
+            this.красныйToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.красныйToolStripMenuItem.Text = "Красный";
+            this.красныйToolStripMenuItem.Click += new System.EventHandler(this.красныйToolStripMenuItem_Click);
+            // 
+            // синийToolStripMenuItem
+            // 
+            this.синийToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.синийToolStripMenuItem.Name = "синийToolStripMenuItem";
+            this.синийToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.синийToolStripMenuItem.Text = "Синий";
+            this.синийToolStripMenuItem.Click += new System.EventHandler(this.синийToolStripMenuItem_Click);
+            // 
+            // зеленыйToolStripMenuItem
+            // 
+            this.зеленыйToolStripMenuItem.BackColor = System.Drawing.Color.Green;
+            this.зеленыйToolStripMenuItem.Name = "зеленыйToolStripMenuItem";
+            this.зеленыйToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.зеленыйToolStripMenuItem.Text = "Зеленый";
+            this.зеленыйToolStripMenuItem.Click += new System.EventHandler(this.зеленыйToolStripMenuItem_Click);
+            // 
+            // желтыйToolStripMenuItem
+            // 
+            this.желтыйToolStripMenuItem.BackColor = System.Drawing.Color.Yellow;
+            this.желтыйToolStripMenuItem.Name = "желтыйToolStripMenuItem";
+            this.желтыйToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.желтыйToolStripMenuItem.Text = "Желтый";
+            this.желтыйToolStripMenuItem.Click += new System.EventHandler(this.желтыйToolStripMenuItem_Click);
+            // 
+            // черныйToolStripMenuItem
+            // 
+            this.черныйToolStripMenuItem.BackColor = System.Drawing.Color.Black;
+            this.черныйToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.черныйToolStripMenuItem.Name = "черныйToolStripMenuItem";
+            this.черныйToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.черныйToolStripMenuItem.Text = "Черный";
+            this.черныйToolStripMenuItem.Click += new System.EventHandler(this.черныйToolStripMenuItem_Click);
+            // 
+            // белыйToolStripMenuItem
+            // 
+            this.белыйToolStripMenuItem.Name = "белыйToolStripMenuItem";
+            this.белыйToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.белыйToolStripMenuItem.Text = "Белый";
+            this.белыйToolStripMenuItem.Click += new System.EventHandler(this.белыйToolStripMenuItem_Click);
+            // 
+            // combo_w
+            // 
+            this.combo_w.FormattingEnabled = true;
+            this.combo_w.Location = new System.Drawing.Point(375, 0);
+            this.combo_w.Name = "combo_w";
+            this.combo_w.Size = new System.Drawing.Size(35, 21);
+            this.combo_w.TabIndex = 1;
+            this.combo_w.SelectedIndexChanged += new System.EventHandler(this.combo_w_SelectedIndexChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(837, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(280, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Толщина  фигур";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.combo_w);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem квадратToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem прямоугольникToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem элипсToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem линияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem цветToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem красныйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem синийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem зеленыйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem желтыйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem черныйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem белыйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem заливкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ComboBox combo_w;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BorysenkoGDI/BorysenkoGDI/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace BorysenkoGDI
+{
+    public partial class Form1 : Form
+    {
+        private BufferedGraphicsContext curCont;
+        private BufferedGraphics myBuf;
+        private Point Begin, End;
+        private int offsetX, offsetY;
+        private bool move = false;
+        public string figure = "";
+        private Color my_color;
+        private int width;
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr SelectObject(IntPtr hdc, IntPtr hgdiobj);
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateSolidBrush(int crColor);
+        [DllImport("gdi32.dll")]
+        public static extern bool ExtFloodFill(IntPtr hdc, int nXStart, int nYStart, int crColor, uint fuFillType);
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+        [DllImport("gdi32.dll")]
+        public static extern int GetPixel(IntPtr hdc, int x, int y);
+
+        enum FLOODFILLTYPE { FLOODFILLBORDER, FLOODFILLSURFACE };
+         
+        public Form1()
+        {
+            InitializeComponent();
+            my_color = Color.Black;
+            Buffer();
+            combo_w.Items.Add("2");
+            combo_w.Items.Add("4");
+            combo_w.Items.Add("6");
+            combo_w.Items.Add("8");
+            combo_w.SelectedIndex=0;
+            //width = int.Parse(combo_w.SelectedText);
+
+        }
+        private void Buffer()
+        {
+            curCont = BufferedGraphicsManager.Current;
+            myBuf = curCont.Allocate(CreateGraphics(), DisplayRectangle);
+            myBuf.Graphics.Clear(Color.White);
+        }
+
+        private void прямоугольникToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            figure = "rectan";
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            myBuf.Render();
+            if (move == true)
+            {
+                End = e.Location;
+
+                Pen myPen = new Pen(my_color,width);
+                offsetX = Math.Abs(End.X - Begin.X);
+                offsetY = Math.Abs(End.Y - Begin.Y);
+                Graphics g = CreateGraphics();
+                switch (figure) { 
+                    case "rectan":
+
+                        
+                g.DrawRectangle(
+                    myPen,
+                    Begin.X < End.X ? Begin.X : End.X,
+                    Begin.Y < End.Y ? Begin.Y : End.Y,
+                    offsetX, offsetY);
+                        break;
+                    case "circulo":
+
+                        
+                        g.DrawEllipse(
+                            myPen,
+                            Begin.X < End.X ? Begin.X : End.X,
+                            Begin.Y < End.Y ? Begin.Y : End.Y,
+                            offsetX, offsetY);
+                        break;
+                    case "line":
+
+                        g.DrawLine(myPen,Begin,End);
+                        break;
+                    case "lastik":
+                        Pen l = new Pen(Color.White, 5);
+                       myBuf.Graphics.DrawRectangle(l,End.X, End.Y,5, 5);
+                        break;
+                    case "pencil":
+
+                        myPen = new Pen(my_color);
+                        myBuf.Graphics.DrawLine(myPen, Begin, End);
+                        Begin= End;
+
+                        break;
+                           
+                
+                }
+               
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Begin = e.Location;
+            move = true;
+            switch (figure) { 
+                case "zaliv":
+                   
+                    ExtFill(myBuf.Graphics, Begin.X, Begin.Y, FLOODFILLTYPE.FLOODFILLSURFACE, my_color);
+                    break;
+                
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            End = e.Location;
+            move = false;
+            Pen myPen = new Pen(my_color,width);
+            switch (figure)
+            {
+                case "rectan":
+
+                    myBuf.Graphics.DrawRectangle(
+                   myPen,
+                   Begin.X < End.X ? Begin.X : End.X,
+                   Begin.Y < End.Y ? Begin.Y : End.Y,
+                   offsetX, offsetY);
+                    break;
+                case "circulo":
+                    myBuf.Graphics.DrawEllipse(
+                   myPen,
+                   Begin.X < End.X ? Begin.X : End.X,
+                   Begin.Y < End.Y ? Begin.Y : End.Y,
+                   offsetX, offsetY);
+                    break;
+                case "line":
+
+                    myBuf.Graphics.DrawLine(myPen, Begin, End);
+                    break;
+
+
+            }
+           
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            myBuf.Render();
+        }
+
+        private void красныйToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            my_color = Color.Red;
+            toolStripMenuItem1.BackColor = System.Drawing.Color.Red;
+        }
+
+        private void синийToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            my_color = Color.DarkBlue;
+            toolStripMenuItem1.BackColor = System.Drawing.Color.DarkBlue;
+
+        }
+
+        private void зеленыйToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            my_color = Color.DarkGreen;
+            toolStripMenuItem1.BackColor = System.Drawing.Color.DarkGreen;
+
+        }
+
+        private void желтыйToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            my_color = Color.Yellow;
+            toolStripMenuItem1.BackColor = System.Drawing.Color.Yellow;
+
+        }
+
+        private void черныйToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            my_color = Color.Black;
+            toolStripMenuItem1.BackColor = System.Drawing.Color.Black;
+        }
+
+        private void белыйToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            my_color = Color.White;
+            toolStripMenuItem1.BackColor = System.Drawing.Color.White;
+        }
+
+        private void элипсToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            figure = "circulo";
+        }
+
+        private void линияToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            figure = "line";
+        }
+
+        private void заливкаToolStripMenuItem_Click(object sender, System.EventArgs e)
+        {
+            figure = "zaliv";
+        }
+        private void ExtFill(Graphics vGraphics, int x, int y, FLOODFILLTYPE floodFillType, Color fillColor, Color borderColor = default(Color))
+        {
+            IntPtr vDC = vGraphics.GetHdc();
+            IntPtr vBrush = CreateSolidBrush(ColorTranslator.ToWin32(fillColor));
+            IntPtr vPreviouseBrush = SelectObject(vDC, vBrush);
+
+            switch (floodFillType)
+            {
+                case FLOODFILLTYPE.FLOODFILLSURFACE:
+                    ExtFloodFill(vDC, x, y, GetPixel(vDC, x, y), (uint)FLOODFILLTYPE.FLOODFILLSURFACE);
+                    break;
+                case FLOODFILLTYPE.FLOODFILLBORDER:
+                    ExtFloodFill(vDC, x, y, ColorTranslator.ToWin32(borderColor), (uint)FLOODFILLTYPE.FLOODFILLBORDER);
+                    break;
+            }
+            SelectObject(vDC, vPreviouseBrush);
+            DeleteObject(vBrush);
+            vGraphics.ReleaseHdc(vDC);
+        }
+
+        private void toolStripMenuItem2_Click(object sender, System.EventArgs e)
+        {
+            figure = "lastik";
+        }
+
+        private void toolStripMenuItem3_Click(object sender, System.EventArgs e)
+        {
+            figure = "pencil";
+        }
+
+        private void combo_w_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            int temp = combo_w.SelectedIndex;
+            width = (temp + 1) * 2;
+            
+        }
+
+        private void label1_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+    }
+}
